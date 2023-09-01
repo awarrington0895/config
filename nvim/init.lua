9eedf9d607ff82285ef815f839a924fe2c50a2b3
Alex Warrington <awarrington0895@gmail.com>
2023-09-01 18:35:35 +0000

    Add cpp support

--- nvim/init.lua
+++ nvim/init.lua
@@ -280,7 +280,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = {'vim', 'vimdoc', 'lua' },
+  ensure_installed = {'vim', 'vimdoc', 'lua', 'cpp' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -408,6 +408,7 @@ local servers = {
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  clangd = {},
 
   lua_ls = {
     Lua = {
